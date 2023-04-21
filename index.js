75e3ec63efcc4dbcd0dedbec4ca379520b918dab
Serquand <esteban.vincent@efrei.net>
2023-04-21 09:59:40 +0200

    Setup alright

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
-const { Client } = require("discord.js");
+const { Client, Collection } = require("discord.js");
 const setup = require("./models/setup.js");
+const commandHandlers = require("./utils/handlers/CommandHandlers");
+const eventHandlers = require("./utils/handlers/EventHandlers");
 
 const client = new Client({ intents: 1 });
+client.commands = new Collection();
+client.login(process.env.botToken);
 
 const main = async () => {
     console.clear();
     await setup();
+    await commandHandlers(client);
+    await eventHandlers(client);
 }
 
 main();
